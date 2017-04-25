4340e53c2ec9c94b4cad6200e68177e6a6994416
SparkCode <iseb@outlook.com>
2017-04-25 19:36:10 +0400

    some changes

--- js/customScripts.js
+++ js/customScripts.js
@@ -17,52 +17,167 @@ var configurateButton = {
     },
 
     handleButtonClick: function () {
-        var sessionTimeAmount =  session.getValue();
-        var breakTimeAmount = _break.getValue();
+        var sessionAlarm = new Alarm(session.getValue() * 1000 * 60, "Session");
+        var breakAlarm = new Alarm(_break.getValue() * 1000 * 60, "Break");
+        pomodoroWorker.init(sessionAlarm, breakAlarm);
+        pomodoroWorker.prepare();
     }
 };
 
+var pomodoroWorker = {
+    init: function (sessionAlarm, breakAlarm) {
+        !this.sessionAlarm || this.sessionAlarm.stop();
+        !this.breakAlarm || this.breakAlarm.stop();
 
+        this.sessionAlarm = sessionAlarm;
+        this.breakAlarm = breakAlarm;
+        this.state = pomodoroWorker.BREAK;
+    },
 
-function Timer(element, min, max, _default) {
+    start: function () {
+        this.getCurrentAlarm().setAlarm(this.prepare.bind(this), this.updaterHandler.bind(this), 1000);
+    },
 
-    function validateValue (value) {
-        value = +value;
-        var isValid = value && value >= min && value <= max;
-        if (!isValid)
+    prepare: function () {
+        if (this.state === pomodoroWorker.SESSION)
         {
-            setElementValue(_default);
-            throw new UserError("Number should be more than " + min + " and less than " + max);
+            notification.setNotification(Notification.SESSION_DONE);
+            this.state = pomodoroWorker.BREAK;
         }
-    }
+        else
+        {
+            notification.setNotification(Notification.SESSION_STARTED);
+            this.state = pomodoroWorker.SESSION;
+        }
+        alarmNameLabel.setValue(this.getCurrentAlarm().name);
+        alarmDurationLabel.setValue(this.getCurrentAlarm().duration);
+        this.start();
+    },
 
-    function getElementValue() {
-        return $(element).val();
+    updaterHandler: function () {
+        var alarm = this.getCurrentAlarm();
+        var percentage = Math.round(new Date() - alarm.startTime) / (alarm.duration / 100);
+        progressBar.setValue(percentage);
+    },
+
+    getCurrentAlarm : function () {
+        return this.state ===  pomodoroWorker.SESSION ? this.sessionAlarm : this.breakAlarm;
     }
+};
+
+pomodoroWorker.BREAK = 1;
+pomodoroWorker.SESSION = 2;
+
+
+
+var progressBar = {
+    init : function () {
+        this.element = $(".progress-bar")[0];
+    },
     
-    function setElementValue(value) {
-        $(element).val(value);
+    setValue : function (percentage) {
+        this.element.style.width = percentage + "%";
+    }
+};
+
+var alarmNameLabel = {
+    init : function () {
+        this.element = $("#alarm-name")[0];
+    },
+
+    setValue : function (name) {
+        this.element.innerHTML = name;
+    }
+};
+
+var alarmDurationLabel = {
+    init: function () {
+        this.element = $("#alarm-duration")[0];
+    },
+    
+    setValue: function (ms) {
+        this.element.innerHTML  = new Date(0, 0,0,0,0,0, ms).toTimeString().split(' ')[0];
     }
 
-    this.setValue = function (value) {
-        validateValue(value);
-        setElementValue(value);
-    };
+};
 
-    this.changeValue = function (diff) {
-        debugger;
-        var curr = this.getValue();
-        this.setValue(+curr + diff);
-    };
+var currentAlarm = null;
 
-    this.getValue = function () {
-        var value =  +getElementValue();
-        validateValue(value);
-        return value;
-    }
+
+
+function Alarm(duration, name) {
+    this.duration = duration;
+    this.name = name;
+
 }
 
+Alarm.prototype.setAlarm = function (finishFunction, setUpdaterFunction, setUpdaterFunctionDuration) {
+    var self = this;
 
+    this.startTime = new Date();
+
+    this.overallTimerId = setTimeout(function () {
+        self.stop();
+        debugger;
+        finishFunction();
+    }, this.duration);
+
+    this.updaterFunctionId = setInterval(setUpdaterFunction.bind(this), setUpdaterFunctionDuration);
+};
+
+Alarm.prototype.stop = function () {
+    clearTimeout(this.updaterFunctionId);
+    clearTimeout(this.overallTimerId);
+};
+
+
+function Timer(element) {
+    this.element = element;
+}
+
+Timer.prototype.getElementValue = function (){
+    return $(this.element).val();
+};
+
+Timer.prototype.setElementValue = function (value) {
+    $(this.element).val(value);
+};
+
+function ValidatedTimer(element, min, max, _default) {
+    Timer.apply(this, arguments);
+    this.min = min;
+    this.max = max;
+    this.default = _default;
+}
+
+ValidatedTimer.prototype = Object.create(Timer.prototype);
+ValidatedTimer.prototype.constructor = ValidatedTimer;
+
+ValidatedTimer.prototype.setValue = function (value) {
+    this.validateValue(value);
+    this.setElementValue(value);
+};
+
+ValidatedTimer.prototype.changeValue = function (diff) {
+    var curr = this.getValue();
+    this.setValue(+curr + diff);
+};
+
+ValidatedTimer.prototype.getValue = function () {
+    var value = +this.getElementValue();
+    this.validateValue(value);
+    return value;
+};
+
+ValidatedTimer.prototype.validateValue = function (value) {
+    value = +value;
+    var isValid = value && value >= this.min && value <= this.max;
+    if (!isValid)
+    {
+        this.setElementValue(this.default);
+        throw new UserError("Number should be more than " + this.min + " and less than " + this.max);
+    }
+};
 
 function setChangerButton(element, timer, diff) {
     element.onclick = timer.changeValue.bind(timer, diff);
@@ -71,22 +186,76 @@ function setChangerButton(element, timer, diff) {
 function UserError(message) {
     this.name = "UserError";
     this.message = message || 'Default Message';
+    this.stack = new Error().stack;
 }
 
 UserError.prototype = Object.create(Error.prototype);
 UserError.prototype.constructor = UserError;
 
+function Notification(elementStateMap, alertPlaceElement) {
+    this.elementStateMap = elementStateMap;
+    this.alertPlaceElement = alertPlaceElement;
+}
+
+Notification.prototype.setNotification = function (state) {
+    this._closeAllNotification();
+    this.alertPlaceElement.appendChild(this.elementStateMap[state]);
+
+};
+
+Notification.prototype._closeAllNotification = function () {
+    this.alertPlaceElement.innerHTML = "";
+};
+
+Notification.SESSION_DONE = 1;
+Notification.SESSION_STARTED = 2;
+Notification.TIMER_STOPPED = 3;
+Notification.USER_ERROR_HAPPENED = 4;
+
+var session;
+var _break;
+var notification;
+
+
+
+
+
 function init() {
     configurateButton.init();
+    progressBar.init();
+    alarmNameLabel.init();
+    alarmDurationLabel.init();
     var sessionElement = document.getElementById("session-time-amount");
     var breakElement = document.getElementById("break-time-amount");
-    var session = new Timer(sessionElement, 5, 99, 30);
-    var _break = new Timer(breakElement, 1, 99, 5);
+    session = new ValidatedTimer(sessionElement, 1, 99, 30);
+    _break = new ValidatedTimer(breakElement, 1, 99, 5);
+
     setChangerButton($("#session-incrementer")[0], session, +1);
     setChangerButton($("#session-decrementer")[0], session, -1);
     setChangerButton($("#break-incrementer")[0], _break, +1);
     setChangerButton($("#break-decrementer")[0], _break, -1);
+
+    var typeStateMap = {};
+    typeStateMap[Notification.SESSION_DONE] = "done-session-alert";
+    typeStateMap[Notification.SESSION_STARTED] = "started-session-alert";
+    typeStateMap[Notification.TIMER_STOPPED] = "stopped-timer-alert";
+    typeStateMap[Notification.USER_ERROR_HAPPENED] = "error-alert";
+
+
+    var elementStateMap = Object.keys(typeStateMap)
+        .map(function (type) {
+        var el = $(`#alert-templates div[data-type=\'${typeStateMap[type]}\']`)[0];
+        return [type, el];
+    })
+        .reduce(function (acc, curr) {
+            acc[curr[0]] = curr[1];
+            return acc;
+    }, {});
+
+    notification = new Notification(elementStateMap, $("#alert-place")[0]);
 }
+
+
 
 
 
